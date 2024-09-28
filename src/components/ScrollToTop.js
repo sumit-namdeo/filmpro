d6c7676b1bb55cab709fc7a6138adabc25b8c5d7
sumit <smtnamdeo333@gmail.com>
2024-09-28 15:39:15 +0530

    fixing scrolltop

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useLocation, withRouter } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export default function ScrollToTop() {
     const { pathname } = useLocation();
